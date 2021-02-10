3d148e831016fc7da948e92277d3d142a40efa05
kannaig <lmacbeth22@gmail.com>
2021-02-10 19:33:57 +0100

    correction centrage des dessins des dossiers yes no lookaround et circular_movement_head

---
+++ extract_frames_horizontal.py
@@ -0,0 +1,27 @@
+import sys
+import PIL
+from PIL import Image
+
+
+if len(sys.argv) == 1:
+	print("usage: python3 extract_frames.py imgfile nbofimages [outputprefix]");
+else:
+	inputpng= sys.argv[1]
+	print("loading " + inputpng)
+	im = Image.open(inputpng)
+
+	n = int(sys.argv[2])
+	print("to be cut in " + str(n) + " images");
+
+	if len(sys.argv) == 3:
+		outputpngprefix = inputpng[:-4]
+	else:
+		outputpngprefix = sys.argv[3]
+
+	width, height = im.size 
+	  
+	for i in range(0,n):
+            im1 = im.crop((i*width/n, 0, (i+1)*width/n, height)) 
+            im1.save(outputpngprefix + str(i) + ".png")
+            
+	print("done!")
